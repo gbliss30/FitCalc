b10b1f0cd931cf05444f3e6ce8aa51a1ea0a4376
Gabriel Bliss <gabriel.r.bliss@gmail.com>
2021-08-25 17:42:27 -0700

    first commit

---
+++ scripts/mainENG.js
@@ -0,0 +1,37 @@
+//inputs
+const weightEng = document.querySelector('#weightEng');
+const heightEng = document.querySelector('#heightEng');
+const ageEng = document.querySelector('#ageEng');
+const genderEng = document.querySelectorAll('input[name="genderEng"]');
+const submitEng = document.querySelector('#submitEng');
+//outputs
+const bmiOutput = document.querySelector('#BMI');
+const bmrOutput = document.querySelector('#BMR');
+
+submitEng.addEventListener('click', e => {
+    e.preventDefault();
+    const selectedGender = genderEng[0].checked ? "male" : "female";
+
+    bmiOutput.innerHTML = calcBMIEng(weightEng.value, heightEng.value);
+});
+
+function calcBMIEng(weight, height) {
+    const bmi = (weight / (height * height) *703).toFixed(1);
+    let classBMI;
+
+    if (bmi >= 40) {
+        classBMI = 'Obese Class 3';
+    } else if (bmi >= 35.0 && bmi <= 39.9) {
+        classBMI = 'Obese Class 2';
+    } else if (bmi >= 30.0 && bmi <= 34.9) {
+        classBMI = 'Obese Class 1';
+    } else if (bmi >= 25.0 && bmi <= 29.9) {
+        classBMI = 'Overweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        classBMI = 'Normal Range';
+    } else if (bmi < 18.5) {
+        classBMI = 'Underweight';
+    }
+
+    return `BMI: ${bmi}, ${classBMI}`;
+}
